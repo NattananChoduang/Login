c47d9b626108e0cb6ee6946145d7852a3650d53a
NattananChoduang <nattanan.cho@dome.tu.ac.th>
2024-10-18 17:35:58 +0700

    initial node.js web project

---
+++ Frontend/public/js/script.js
@@ -0,0 +1,37 @@
+function submitLogin() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('https://restapi.tu.ac.th/api/v1/auth/Ad/verify', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Application-Key': 'TU4ba678bb05a283a9e002b68b66e8ceff20fa793b143ce659f2c3764e60e8107284f558619493b08f04abca983950bc59'
+        },
+        body: JSON.stringify({ username, password })
+    })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('message').innerText = data.message;
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+
+function call_REST_API_Hello() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const url = (
+        'http://localhost:8080/hello?' +
+        new URLSearchParams({ myName: username, lastName: password}).toString()
+      );
+    
+    fetch(url)
+    .then(response => response.text())
+    .then(text => {
+        document.getElementById('message').innerText = text;
+    })
+    .catch(error => console.error('Error:', error));
+}
